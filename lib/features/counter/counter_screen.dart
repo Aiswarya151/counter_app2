afc691a130b7eb0854820ac5472a14b846487064
Aiswarya151 <aiswaryashaji55@gmail.com>
2025-04-11 12:39:34 +0530

    created counter screen and implemented counter incrementing functionality using riverpod

---
+++ lib/features/counter/counter_screen.dart
@@ -0,0 +1,24 @@
+import 'package:counter_app2/features/counter/viewmodel/counter_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CounterScreen extends ConsumerWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counterProvider);
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter')),
+      body: Center(
+        child: Text('Count: $count', style: TextStyle(fontSize: 20)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(counterProvider.notifier).increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
